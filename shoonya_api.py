e225298d0caa897d9f08a25c84c8060c2f23dc16
ankeshk554-bit <ankeshk554@gmail.com>
2026-05-10 20:32:54 +0530

    Create shoonya_api.py

---
+++ shoonya_api.py
@@ -0,0 +1,19 @@
+class ShoonyaAPI:
+    def __init__(self):
+        pass
+
+    def login(self, userid, password, twoFA, vendor_code, api_secret):
+        # your login logic
+        pass
+
+    def start_websocket(self, callback):
+        # your websocket logic
+        pass
+
+    def download_master(self, exchange):
+        # your master file logic
+        pass
+
+    def place_order(self, **kwargs):
+        # your order placement logic
+        pass
